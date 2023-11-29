f0c2bc527a66ea76b2da3fd48433acbee7a22d92
Komron <kr135369@gmail.com>
2023-11-29 12:38:20 +0500

    objects/array

---
+++ js fundamentals_2/objects/index.js
@@ -0,0 +1,21 @@
+//Array
+
+const jonasArray = [
+  'Jonas',
+  'Shmedtman',
+  2037 - 1991,
+  'teacher',
+  ['Michael, Peter, Steven'],
+];
+
+//Object
+
+const jonas = {
+  name: 'Jonas',
+  firstName: 'Shmedtman',
+  birthYeah: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael, Peter, Steven'],
+};
+
+// In objects we have key
